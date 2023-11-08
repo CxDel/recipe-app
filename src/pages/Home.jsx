5ce8b245331920ca60bd5cafcda47f9bdea79d90
cxdel <chris.delirosa@gmail.com>
2023-11-07 22:46:08 -0800

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Veggie from "../components/Veggie";
+import Popular from "../components/Popular";
+
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>Home Page</h1>
+        <Veggie />
+        <Popular />
+
+    </div>
+  )
+}
+
+export default Home
